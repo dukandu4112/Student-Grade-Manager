68e9d5342ce0273681d4e2120a74ab89a706d508
dukandu4112 <dukandu4112@gmail.com>
2026-06-30 22:12:49 +0000

    Refactor menu into Menu class

--- src/Main.java
+++ src/Main.java
@@ -11,21 +11,7 @@ public class Main {
         int choice;
 
         do {
-            System.out.println("\n===== Student Grade Manager =====");
-            System.out.println("1. Add student");
-            System.out.println("2. Add grade");
-            System.out.println("3. Show average grade");
-            System.out.println("4. Show all students");
-            System.out.println("5. Edit student grade");
-            System.out.println("6. Delete student");
-            System.out.println("7. Search student");
-            System.out.println("8. Show highest grade");
-            System.out.println("9. Show lowest grade");
-            System.out.println("10. Sort students by name");
-            System.out.println("11. Sort students by highest grade");
-            System.out.println("12. Sort students by lowest grade");
-            System.out.println("13. Exit");
-            System.out.print("Enter your choice: ");
+            Menu.displayMenu();
 
             choice = input.nextInt();
             input.nextLine();
